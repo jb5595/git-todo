f85b1f360cf3993854eb04087869f402aad139ce
jb5595 <jacobberman1995@gmail.com>
2018-12-08 16:23:14 -0500

    moved other project out of folder

--- Documents/Mod-5-final-project-frontend/mod-5-final-project-frontend/src/reducers/BrowserPropsReducer.js
+++
@@ -1,9 +0,0 @@
-export default function ManageBrowser(state = { BrowserProps: null }, action) {
-    switch (action.type) {
-
-    case "SET_BROWSER_PROPS":
-      return {BrowserProps: action.props}
-    default:
-      return state
-  }
-}
